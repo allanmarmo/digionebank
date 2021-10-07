ccfba3a757b427d6683b3feb9e96538f3a5076f7
allanmarmo <allanmarmo@gmail.com>
2021-10-07 17:57:25 -0300

    created person class

--- src/main/kotlin/one/digitalinnovaion/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovaion/digionebank/Pessoa.kt
@@ -1,4 +1,13 @@
 package one.digitalinnovaion.digionebank
 
 class Pessoa {
+    var nome: String = "Allan"
+    var cpf: String = "123.123.123-11"
+}
+
+fun main() {
+    val allan = Pessoa()
+
+    println(allan.nome)
+    println(allan.cpf)
 }
